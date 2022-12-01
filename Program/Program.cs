db25d2b074f3f18daf2af27c8d7bdbec395138e3
P.S.Pokalnetov <redx@mail.ru>
2022-12-01 15:31:19 +0500

    mtd. GetModArray + исходные данные

--- Program/Program.cs
+++ Program/Program.cs
@@ -2,12 +2,16 @@
 {
     private static void Main(string[] args)
     {
-
+        string[][] myArray = new string[][]
+        {
+            new string[] { "hello", "2", "world", ":-)"},
+            new string[] { "1234", "1567", "-2", "computer science" },
+            new string[] { "Russia", "Denmark", "Kazan" }
+            };
     }
 
     private static void PrintArray(string[] array)
     {
-
         int arrayLen = array.Length - 1;
         if (arrayLen < 0)
         {
@@ -20,6 +24,23 @@
             Console.Write($"\"{array[i]}\", ");
         }
         Console.Write($"\"{array[arrayLen]}\"]");
+    }
+
+    private static string[] GetModArray(string[] modArray)
+    {
+        int arrayLen = modArray.Length;
+        int count = 0;
+        string[] tempArray = new string[arrayLen];
+        for (int i = 0; i < arrayLen; i++)
+        {
+            if (modArray[i].Length < 4)
+            {
+                tempArray[count++] = modArray[i];
+            }
+        }
+        string[] resultArray = new string[count];
+        Array.Copy(tempArray, resultArray, count);
+        return resultArray;
     }
 }
 
