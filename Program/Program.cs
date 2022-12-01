ff23975212068a6447125723bc5c15d87d8ec3ef
P.S.Pokalnetov <redx@mail.ru>
2022-12-01 15:25:22 +0500

    mtd. PrintArray

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,5 +4,22 @@
     {
 
     }
+
+    private static void PrintArray(string[] array)
+    {
+
+        int arrayLen = array.Length - 1;
+        if (arrayLen < 0)
+        {
+            Console.WriteLine("[]");
+            return;
+        }
+        Console.Write("[");
+        for (int i = 0; i < arrayLen; i++)
+        {
+            Console.Write($"\"{array[i]}\", ");
+        }
+        Console.Write($"\"{array[arrayLen]}\"]");
+    }
 }
 
